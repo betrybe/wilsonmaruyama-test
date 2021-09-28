38d483c64a22ce2d56715e567920c6513dc06c52
psmw <will.msdp@gmail.com>
2021-09-28 20:11:41 -0300

    autenticacao ok e inicio da estilizacao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+
+import './styles/Login.css';
 
 function Login() {
   const [formEmail, setFormEmail] = useState('');
   const [formPassword, setFormPassword] = useState('');
+  const history = useHistory();
 
   function validateForm() {
     const minPass = 6;
-    return formEmail.length > 0 && formPassword.length > minPass;
+    const regexFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (regexFilter.test(formEmail) && formPassword.length > minPass) {
+      return true;
+    }
+  }
+
+  function loginPage() {
+    const newUrl = '/carteira';
+    history.push(newUrl);
+    console.log(formEmail);
+    console.log(formPassword);
+    return null;
   }
 
   return (
@@ -32,7 +47,13 @@ function Login() {
             onChange={ (e) => setFormPassword(e.target.value) }
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={ !validateForm() }>
+        <Button
+          block
+          size="lg"
+          type="submit"
+          disabled={ !validateForm() }
+          onClick={ loginPage }
+        >
           Login
         </Button>
       </Form>

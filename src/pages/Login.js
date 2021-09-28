84d0c89e2ff1295e3093ac611b18a72d2d55e2f4
psmw <will.msdp@gmail.com>
2021-09-28 18:07:17 -0300

    formulario de login preliminar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
+function Login() {
+  const [formEmail, setFormEmail] = useState('');
+  const [formPassword, setFormPassword] = useState('');
+
+  function validateForm() {
+    const minPass = 6;
+    return formEmail.length > 0 && formPassword.length > minPass;
   }
+
+  return (
+    <div className="login-holder">
+      <Form onSubmit={ console.log('ok') }>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={ formEmail }
+            onChange={ (e) => setFormEmail(e.target.value) }
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={ formPassword }
+            onChange={ (e) => setFormPassword(e.target.value) }
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={ !validateForm() }>
+          Login
+        </Button>
+      </Form>
+      <Link to="/carteira">teste</Link>
+    </div>
+  );
 }
 
 export default Login;
